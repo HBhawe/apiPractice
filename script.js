23f7be44144341cf31419971e9518f3df880a12d
Harshal Bhawe <harshalb97@outlook.com>
2025-04-08 12:25:45 +0200

    Updated to try catch and async operation

--- script.js
+++ script.js
@@ -1,18 +1,73 @@
-"use strict"
+"use strict";
+
+let env = "PROD";
 
 // FREE API TIER
 // API KEY AVAILABLE ON "https://www.visualcrossing.com/resources/documentation/weather-api/timeline-weather-api/#request-base-url"
-const URL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
 
-const unitGroup = "metric"
-const contentType = "json"
+// API SETUP
+const API_KEY = "N23MVHB8C2TU7AX9H2YD2F89E";
+const URL =
+  "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+const unitGroup = "metric";
+const contentType = "json";
 
-let callPath = `${URL}Uppsala?key=${API_KEY}&unitGroup=${unitGroup}&contentType=${contentType}`;
+// QUERY SELECTORS
+const container = document.querySelector("#container");
+const btnFetch = document.querySelector("#btnFetch");
+const weatherForm = document.querySelector("#weatherForm");
+const weatherResult = document.querySelector("#weatherResult");
 
-// console.log(callPath);
+// FUNCTIONS
+const renderResult = function (result) {
+  const markup = `<p>Address: ${result.resolvedAddress}</p><p>Latitude: ${result.latitude}, Longitude: ${result.longitude}, Description: ${result.description}</p><p>Current Temperature: ${result.currentConditions.temp} C</p>`;
+  weatherResult.insertAdjacentHTML("beforeend", markup);
+};
 
-let response = fetch(`${URL}Uppsala?key=${API_KEY}&unitGroup=${unitGroup}&contentType=${contentType}`, {
-    method: 'GET',
-})
-    .then(response => response.json())
-    .then(json => {console.log(json)})
+const renderError = function (result, message) {
+  const markup = `<p>Error in query. Error: ${result.message}</p>`;
+  weatherResult.insertAdjacentHTML("beforeend", markup);
+};
+
+const fetchData = async function (url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const json = await response.json();
+    console.log(json);
+    renderResult(json);
+  } catch (error) {
+    console.error(error.message);
+    renderError(error);
+  }
+};
+
+const formSubmit = function (form, submitter) {
+  let obj = {};
+  const formData = new FormData(form, submitter);
+  for (const [key, value] of formData) {
+    obj[key] = value;
+  }
+  return obj;
+};
+
+// EVENT LISTENERS
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let data = formSubmit(weatherForm, btnFetch);
+  const place = data.place;
+  if (place === "") {
+    console.log(`No value given`);
+  } else {
+    const callPath = `${URL}${place}?key=${API_KEY}&unitGroup=${unitGroup}&contentType=${contentType}`;
+    if (env === "TEST") {
+      console.log(`test environment: ${env}`);
+    } else {
+      weatherResult.innerHTML = "";
+      fetchData(callPath);
+      weatherForm.reset();
+    }
+  }
+});
