bbeef46f219e568ec56c5a6a26a9702918bf9fe1
Harshal Bhawe <harshalb97@outlook.com>
2025-04-08 12:48:34 +0200

    Updated styling

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let env = "PROD";
 // API SETUP
 const API_KEY = "N23MVHB8C2TU7AX9H2YD2F89E";
 const URL =
-  "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
 const unitGroup = "metric";
 const contentType = "json";
 
@@ -20,54 +20,53 @@ const weatherResult = document.querySelector("#weatherResult");
 
 // FUNCTIONS
 const renderResult = function (result) {
-  const markup = `<p>Address: ${result.resolvedAddress}</p><p>Latitude: ${result.latitude}, Longitude: ${result.longitude}, Description: ${result.description}</p><p>Current Temperature: ${result.currentConditions.temp} C</p>`;
-  weatherResult.insertAdjacentHTML("beforeend", markup);
+    const markup = `<p class="title">Address:</p><p>${result.resolvedAddress}</p><p class="title">Latitude:</p> <p>${result.latitude}</p><p class="title">Longitude:</p><p>${result.longitude}</p><p class="title">Description:</><p>${result.description}</p><p class="title">Current Temperature:</p><p>${result.currentConditions.temp} \u00B0 C</p>`;
+    weatherResult.insertAdjacentHTML("beforeend", markup);
 };
 
 const renderError = function (result, message) {
-  const markup = `<p>Error in query. Error: ${result.message}</p>`;
-  weatherResult.insertAdjacentHTML("beforeend", markup);
+    const markup = `<p>Error in query. Error: ${result.message}</p>`;
+    weatherResult.insertAdjacentHTML("beforeend", markup);
 };
 
 const fetchData = async function (url) {
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        const json = await response.json();
+        renderResult(json);
+    } catch (error) {
+        console.error(error.message);
+        renderError(error);
     }
-    const json = await response.json();
-    console.log(json);
-    renderResult(json);
-  } catch (error) {
-    console.error(error.message);
-    renderError(error);
-  }
 };
 
 const formSubmit = function (form, submitter) {
-  let obj = {};
-  const formData = new FormData(form, submitter);
-  for (const [key, value] of formData) {
-    obj[key] = value;
-  }
-  return obj;
+    let obj = {};
+    const formData = new FormData(form, submitter);
+    for (const [key, value] of formData) {
+        obj[key] = value;
+    }
+    return obj;
 };
 
 // EVENT LISTENERS
 weatherForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  let data = formSubmit(weatherForm, btnFetch);
-  const place = data.place;
-  if (place === "") {
-    console.log(`No value given`);
-  } else {
-    const callPath = `${URL}${place}?key=${API_KEY}&unitGroup=${unitGroup}&contentType=${contentType}`;
-    if (env === "TEST") {
-      console.log(`test environment: ${env}`);
+    e.preventDefault();
+    let data = formSubmit(weatherForm, btnFetch);
+    const place = data.place;
+    if (place === "") {
+        console.log(`No value given`);
     } else {
-      weatherResult.innerHTML = "";
-      fetchData(callPath);
-      weatherForm.reset();
+        const callPath = `${URL}${place}?key=${API_KEY}&unitGroup=${unitGroup}&contentType=${contentType}`;
+        if (env === "TEST") {
+            console.log(`test environment: ${env}`);
+        } else {
+            weatherResult.innerHTML = "";
+            fetchData(callPath);
+            weatherForm.reset();
+        }
     }
-  }
 });
